9d0546dba9b15d9040819b7e24f1a6b7dd1b0c6f
ScorpioGameKing <scorpiogameking@gmail.com>
2026-07-24 21:40:30 -0400

    trying with nixpkgs

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
         modules = [ ./modules/nixos/programs/cli/nvf/nvf.nix ];
       }).neovim;
 
-    packages."x86_64-linux".steam = stdenv.mkDerivation {
+    packages."x86_64-linux".steam = nixpkgs.stdenv.mkDerivation {
       name = "steam";
       src = ./modules/nixos/programs/gui/steam/steam.nix;
     };
